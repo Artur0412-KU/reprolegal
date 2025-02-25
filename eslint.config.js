c9a9dc657db4153c55874889841a2a6674f5a3ad
Artur Kudyrko <kudyrkoartur@gmail.com>
2025-02-25 23:42:47 +0200

    Fixed build bugs

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,38 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { ESLint } from 'eslint';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+export default {
+    ignores: ['dist'],
+    extends: [
+        js.configs.recommended,
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+        ecmaVersion: 2020,
+        globals: globals.browser,
+        parser: '@typescript-eslint/parser',  // TypeScript парсер
+        parserOptions: {
+            project: './tsconfig.json',  // шляхи до вашого tsconfig.json, якщо потрібно
+        },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+        '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+        ...reactHooks.configs.recommended.rules,
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        '@typescript-eslint/no-explicit-any': 'warn', // приклад додаткових правил для TypeScript
     },
-  },
-)
+};
+
--- src/components/Slider/Testmonials.tsx
+++ src/components/Slider/Testmonials.tsx
@@ -1,5 +1,5 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import "swiper/css";
 import "swiper/css/pagination";
 import {Pagination} from "swiper/modules";
 import {useTranslation} from "react-i18next";
